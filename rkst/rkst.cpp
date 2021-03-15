b8611d5f3f510b66b0660502308964a5fdf0a6e1
Иван Чамкин <chamkinivan39@gmail.com>
2021-03-15 19:08:28 +0300

    v1

---
+++ rkst/rkst.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int a, b;
+
+	cout << "Введите размер массива: " << endl;
+	
+	while (1 == 1)
+	{
+		cin >> a >> b;
+		if (a > 0 && b > 0)
+		{
+			break;
+		}
+		else
+		{
+			cout << "Вы ввели неверные значения, повторите: " << endl;
+		}
+
+	}
+	int** oldarr = new int* [a];
+	int** newarr = new int* [a];
+	int* buf;
+
+	cout << "Исходная матрица: " << endl;
+	for (int i = 0; i < a; i++)
+	{
+		oldarr[i] = new int[b];
+
+		for (int j = 0; j < b; j++)
+		{
+			oldarr[i][j] = rand() % 20;
+			cout << oldarr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+
+	cout << endl << "Зеркальная матрица: " << endl;
+	for (int i = 0; i < a; i++)
+	{
+		newarr[i] = new int[b];
+
+		for (int j = 0; j < b; j++)
+		{
+			newarr[i][j] = oldarr[a - i - 1][j];
+			cout << newarr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < a; i++)
+	{
+		delete[] oldarr[i];
+		delete[] newarr[i];
+	}
+
+	delete[] oldarr;
+	delete[] newarr;
+	
+
+	return 0;
+}
